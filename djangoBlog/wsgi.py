a5a5db8c74a6d7c029a1ef7624e2e6c866426efd
Superficial <35603504+SuperficialL@users.noreply.github.com>
2019-03-25 06:13:01 +0800

    修改配置文件uwsgi

--- djangoBlog/wsgi.py
+++ djangoBlog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalBlog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBlog.settings')
 
 application = get_wsgi_application()
