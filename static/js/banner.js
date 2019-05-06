15d52e762934ff27d13565e69d0cb6bed15a2c3b
SuperficialL <347106739@qq.com>
2019-05-06 22:21:16 +0800

    轮播修改为swiper插件

--- static/js/banner.js
+++ static/js/banner.js
@@ -1,58 +1,17 @@
 $(function () {
-    let curIndex = 0,
-        slider = $(".carousel-inner"),
-        totalSlides = $(".carousel-inner .item").length,
-        sliderWidth = $(".carousel-inner .item").width(),
-        dots = $(".carousel-indicators li"),
-        btn_prev = $('.prev'),
-        btn_next = $('.next'),
-        timer = null;
-
-    // 设置总宽度
-    slider.width(sliderWidth * totalSlides);
-
-    // 下一张图片
-    btn_next.click(function () {
-        slideRight();
+    var swiper = new Swiper('.carousel', {
+        slidesPerView: 1,
+        autoplay: {
+            delay: 2000,//1秒切换一次
+        },
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
-
-    // 上一张图片
-    btn_prev.click(function () {
-        slideLeft();
-    });
-
-
-    dots.mouseenter(function () {
-        curIndex = $(this).index();
-        $(this).addClass('active').siblings().removeClass("active");
-        slider.animate({'left': -(curIndex * sliderWidth)});
-    });
-
-    // 自动轮播
-    timer = setInterval(slideRight, 3000);
-    slider.hover(
-        function () {
-            clearInterval(timer);
-        }, function () {
-            timer = setInterval(slideRight, 3000);
-        }
-    );
-
-    function slideLeft() {
-        curIndex--;
-        if (curIndex === -1) {
-            curIndex = totalSlides - 1;
-        }
-        slider.animate({'left': -(sliderWidth * curIndex)});
-        dots.removeClass('active').eq(curIndex).addClass('active');
-    }
-
-    function slideRight() {
-        curIndex++;
-        if (curIndex === totalSlides) {
-            curIndex = 0;
-        }
-        slider.animate({'left': -(sliderWidth * curIndex)});
-        dots.removeClass('active').eq(curIndex).addClass('active');
-    }
 });
